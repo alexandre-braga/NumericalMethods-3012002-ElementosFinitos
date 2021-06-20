01a1f5a293255828e3b8d0dbf0ba977d1a14698e
Alexandre Vitor <alexandre.braga@estudante.ufjf.br>
2021-06-20 01:20:16 -0300

    formulaçao Q1 consertada, Q2 com betaIdeal

--- Trab3/Q2/DifusaoConveccaoSUBetaIdeal.m
+++ Trab3/Q2/DifusaoConveccaoSUBetaIdeal.m
@@ -6,24 +6,20 @@ format long;
 %dominio
 a = 0.0;
 b = 1.0;
-erro = zeros(4,1);
-hh = zeros(4,1);
+erroSU = zeros(4,1);
+hhSU = zeros(4,1);
 
 for grau = 1:1
-  for cont = 1:3
+  for cont = 1:1
     Kappa = 1.;
     E = 1.e-2;
-    Peh = 1;
-    if cont >=2
-      Peh = 5;
-      if cont >=3
-        Peh = 10;
-      endif
-    endif
-    %tamanho do elemento
-    h = (Peh*2*E)/abs(Kappa)
     %n de elementos
-    nel = (b-a)/h
+    nel = 10;
+    %tamanho do elemento
+    hSU = (b-a)/nel
+    %beta ideal
+    Peh = hSU*abs(Kappa)/(2*E)
+    Beta = 1 - 1/Peh
     %grau do polinomio de interpolação
     k = grau;
     %n de nós do elemento
@@ -41,28 +37,29 @@ for grau = 1:1
     %vetor fonte zerado
     F = zeros(np,1);
 
-    %montagem do xl !!!LINEAR!!!
-    xl = zeros(np,1);
-    xl(1) = a;
+    %montagem do xl
+    xlSU = zeros(np,1);
+    xlSU(1) = a;
     for i = 2:np
-      xl(i) = xl(i-1) + h/k;
+      xlSU(i) = xlSU(i-1) + hSU/k;
     endfor
 
     %gera shg e pega as funções peso
     [shg, w]= shgGera(nen,nint);
-
+    [shgSU, wSU]= shgGeraSU(nen,nint,Beta);
+        
     %montagem global
     for n = 1:nel
       for l = 1:nint
         xx = 0.;
         for i = 1:nen
-          xx = xx + shg(1,i,l)*xl(k*(n-1)+i);
+          xx = xx + shg(1,i,l)*xlSU(k*(n-1)+i);
         endfor
         for j = 1:nen
-          F(k*(n-1)+j) = F(k*(n-1)+j) + funcao(xx)*shg(1,j,l)*w(l)*h/2;
+          F(k*(n-1)+j) = F(k*(n-1)+j) + funcao(xx)*shg(1,j,l)*w(l)*hSU/2;
           for i = 1:nen
-            K((k*(n-1)+i),(k*(n-1)+j)) = K((k*(n-1)+i),(k*(n-1)+j)) + funcaok(xx,E)*shg(2,j,l)*2/h*shg(2,i,l)*2/h*w(l)*h/2;
-            C((k*(n-1)+j),(k*(n-1)+i)) = C((k*(n-1)+j),(k*(n-1)+i)) + funcaoKappa(xx,Kappa)*shg(2,i,l)*2/h*shg(1,j,l)*w(l)*h/2;
+            K((k*(n-1)+i),(k*(n-1)+j)) = K((k*(n-1)+i),(k*(n-1)+j)) + (funcaok(xx,E) + Beta*Kappa*hSU/2)*shg(2,j,l)*2/hSU*shg(2,i,l)*2/hSU*w(l)*hSU/2;
+            C((k*(n-1)+i),(k*(n-1)+j)) = C((k*(n-1)+i),(k*(n-1)+j)) + funcaoKappa(xx,Kappa)*shg(2,j,l)*2/hSU*shg(1,i,l)*w(l)*hSU/2;
           endfor
         endfor
       endfor
@@ -85,18 +82,18 @@ for grau = 1:1
       KC(i,np) = 0.;
     endfor
     KC(np,np) = 1;
-    F(np) = 0;
+    F(np) = 1;
     
     %função exata
     x = a;
     exata = zeros(np,1);
     for i = 1:np
       exata(i) = funcaoExata(x,E,Kappa);
-      x += h/k;
+      x += hSU/k;
     endfor  
-    x = a:h/k:b;
-    u = zeros(np);
-    u = KC\F;
+    x = a:hSU/k:b;
+    uSU = zeros(np);
+    uSU = KC\F;
 
     %cálculo do erro
     erdul2 = 0;
@@ -106,25 +103,25 @@ for grau = 1:1
         duh = 0;
         xx = 0;
         for i = 1:nen
-          duh = duh + shg(2,i,l)*2/h*u(k*(n-1)+i);
-          xx = xx + shg(1,i,l)*xl(k*(n-1)+i);
+          duh = duh + shg(2,i,l)*2/hSU*uSU(k*(n-1)+i);
+          xx = xx + shg(1,i,l)*xlSU(k*(n-1)+i);
         endfor
-        erdu = erdu + ((dfuncaoExata(xx,E,Kappa) - duh)**2) * w(l) * h/2;
+        erdu = erdu + ((dfuncaoExata(xx,E,Kappa) - duh)**2) * w(l) * hSU/2;
        endfor
        erdul2 = erdul2 + erdu;
      endfor
     erdul2 = sqrt(erdul2);
-    erro(cont) = erdul2;
-    hh(cont) = h;
+    erroSU(cont) = erdul2;
+    hhSU(cont) = hSU;
       
     %salva a resolucao
-    nome = sprintf("log/PesosEPontosIntegracaoPeh%dGrau%d.txt", Peh, grau);
-    save(nome, 'xl', 'h', 'u', 'x', 'exata');
+    nome = sprintf("log/BetaIdealDA%d.txt", Beta);
+    save(nome, 'xlSU', 'hSU', 'uSU', 'x', 'exata');
       
   endfor 
   
   %salva os erros
-  nome = sprintf("log/Erros%d.txt", grau);
-  save(nome, 'erro', 'hh');
+  nome = sprintf("log/ErrosBetaIdealDA%d.txt", grau);
+  save(nome, 'erroSU', 'hhSU');
 
 endfor 
