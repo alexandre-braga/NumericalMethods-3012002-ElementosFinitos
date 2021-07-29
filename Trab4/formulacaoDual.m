bf10c7a4d883def6b33e2a7e062440574f1382d0
Alexandre Vitor <alexandre.braga@estudante.ufjf.br>
2021-07-29 03:35:36 -0300

    encontrar o erro na formualacao e testar pra um contorno

--- Trab4/formulacaoDual.m
+++ Trab4/formulacaoDual.m
@@ -15,7 +15,7 @@ hh = zeros(4,1);
 
 for grau = 1:4
   for cont = 1:4
-    nel = 4^cont;
+    nel = 4*cont;
     h = (b-a)/nel;
     k = grau;
     nen = k+1;
@@ -26,6 +26,7 @@ for grau = 1:4
     B = zeros(np,np);
     C = zeros(np,np);
     
+    G = zeros(np,1);
     F = zeros(np,1);
 
     %montagem do xl
@@ -34,10 +35,8 @@ for grau = 1:4
     for i = 2:np
       xl(i) = xl(i-1) + h/k;
     endfor
-
     %gera shg e pega as funções peso
     [shg, w]= shgGera(nen,nint);
-
     %Montagem da Fonte e da Matriz
     for n = 1:nel
       Ge = zeros(np,1);
@@ -52,57 +51,59 @@ for grau = 1:4
         endfor
         for j = 1:nen
           Ge(k*(n-1)+j) = Ge(k*(n-1)+j) + delta2*funcao(xx)*shg(2,j,l)*2/h*w(l)*h/2;
-          Fe(k*(n-1)+j) = Fe(k*(n-1)+j) + shg(1,j,l)*w(l)*h/2;
+          Fe(k*(n-1)+j) = Fe(k*(n-1)+j) + funcao(xx)*shg(1,j,l)*w(l)*h/2;
           for i = 1:nen
             Ae((k*(n-1)+i),(k*(n-1)+j)) = Ae((k*(n-1)+i),(k*(n-1)+j)) + ((1+delta1)*shg(1,i,l)*shg(1,j,l) + delta2*shg(2,i,l)*2/h*shg(2,j,l)*2/h)*w(l)*h/2;
             Be((k*(n-1)+i),(k*(n-1)+j)) = Be((k*(n-1)+i),(k*(n-1)+j)) + (-shg(1,i,l)*shg(2,j,l)*2/h + delta1*shg(2,i,l)*2/h*shg(2,j,l)*2/h)*w(l)*h/2;
-            Ce((k*(n-1)+j),(k*(n-1)+i)) = Ce((k*(n-1)+j),(k*(n-1)+i)) + delta1*shg(2,i,l)*2/h*shg(2,j,l)*2/h)*w(l)*h/2;
+            Ce((k*(n-1)+j),(k*(n-1)+i)) = Ce((k*(n-1)+j),(k*(n-1)+i)) + delta1*shg(2,i,l)*2/h*shg(2,j,l)*2/h*w(l)*h/2;
           endfor
         endfor
       endfor
-      F(n) = Fe(n);
-      F(n + nel) = Ge(n);
       A = A + Ae;
       B = B + Be;
+      BT = B';
       C = C + Ce;
+      G = G + Ge;
+      F = F + Fe;
     endfor
-    BT = B';
-        
     %Matriz global
     M = zeros(np,np);
+    Fonte = zeros(2*np,1);
     
     %Caso o tamanho difira, usar outros loops
     %E modificar o tamanho da M, o mesmo pra F
     for i = 1:np
       for j = 1:np
         M(i,j) = A(i,j);
-        M(i,j + nel) = B(i,j);
-        M(i + nel,j) = BT(i,j);
-        M(i + nel,j + nel) = C(i,j);
+        M(i,j + np) = B(i,j);
+        M(i + np,j) = BT(i,j);
+        M(i + np,j + np) = C(i,j);
       endfor
+      Fonte(i) = G(i);
+      Fonte(i + np) = F(i);
+    endfor
+    
+    U = M\Fonte;
+    
+    u = zeros(np,1);
+    p = zeros(np,1);
+    for i = 1:np
+      u(i) = U(i);
+      p(i) = U(i + np);
     endfor
     
     %função exata
     x = a;
-    t = T0;
-    j = 1;
-    espacoT = ceil((T-T0)/deltaT + 1)
-    exata = zeros(np,espacoT);
-    while t <= T
-      for i = 1:np
-        exata(i,j) = funcaoExata(x,t,E,Kappa);
-        x += h/k;
-      endfor
-      x = a;
-      j++;
-      t += deltaT;
-    endwhile
+    exata = zeros(np,1);
+    for i = 1:np
+      exata(i) = funcaoExata(x);
+      x += h/k;
+    endfor
     x = a:h/k:b;
-    t = T0:deltaT:T;
- 
+    
     %salva a resolucao
-    nome = sprintf("log/PesosEPontosIntegracaoPeh%dGrau%d.txt", Peh, grau);
-    save(nome, 'xl', 'h', 'deltaT', 'U', 'x', 't', 'exata');
+    nome = sprintf("log/PesosEPontosIntegracao%dGrau%d.txt", cont, grau);
+    save(nome, 'xl', 'h', 'u', 'p', 'x', 'exata');
       
   endfor 
   
