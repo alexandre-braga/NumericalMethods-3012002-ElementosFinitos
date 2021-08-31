76729e980d0d34649ec7bcf8a9ab237125677f19
Alexandre Vitor <alexandre.braga@estudante.ufjf.br>
2021-08-30 21:19:18 -0300

    verificar depois o erro nitsche

--- Trab5/Descontinua/formulacaoDescontinua.m
+++ Trab5/Descontinua/formulacaoDescontinua.m
@@ -75,25 +75,6 @@ for cont = 1:4
   endfor
   x = a:h/k:b;
   
-  %cálculo do erro L2
-  erul2 = 0;
-  for n = 1:nel
-    eru = 0;
-    for l = 1:nint
-      uh = 0;
-      xx = 0;
-      for i = 1:nen
-        uh = uh + shg(1,i,l)*u(i);
-        xx = xx + shg(1,i,l)*xl(k*(n-1)+i);
-      endfor
-      eru = eru + ((funcaoExata(xx) - uh)**2) * w(l) * h/2;
-    endfor
-    erul2 = erul2 + eru;
-  endfor
-  erul2 = sqrt(erul2);
-  erro(cont) = erul2;
-  hh(cont) = h;
-  
   xlU1 = xl(1:end-1);
   xlU2 = xl(2:end);
   
--- Trab5/Nitsche/formulacaoNitsche.m
+++ Trab5/Nitsche/formulacaoNitsche.m
@@ -20,7 +20,7 @@ beta = input('Insira o valor de beta: ');
 erro = zeros(4,1);
 hh = zeros(4,1);
 
-for cont = 1:2
+for cont = 1:4
   nel = 4^cont;
   h = (b-a)/nel;
   k = grau;
@@ -57,28 +57,26 @@ for cont = 1:2
       endfor
     endfor
   endfor
-  A
-  F
+
   %Contorno Nitsche-  
   for i = 1:nen
     if i == 1
-      F(i) += alfa*(shg(2,i,2)*2/h*funcaoExata(xl(n+1)) - shg(2,i,1)*2/h*funcaoExata(xl(n))) + beta*-1*funcaoExata(xl(n+1));
+      F(i) += alfa*(shge(2,i,2)*2/h*funcaoExata(xl(np)) - shge(2,i,1)*2/h*funcaoExata(xl(1)) ) + beta*(funcaoExata(xl(np))*shg(1,i,2)-funcaoExata(xl(1))*shg(1,i,1));
     endif
     if i == nen
-      F(np) += alfa*(shg(2,i,2)*2/h*funcaoExata(xl(n+1)) - shg(2,i,1)*2/h*funcaoExata(xl(n))) + beta*1*funcaoExata(xl(n+1));
+      F(np) += alfa*(shge(2,i,2)*2/h*funcaoExata(xl(np)) - shge(2,i,1)*2/h*funcaoExata(xl(1)) )  + beta*(funcaoExata(xl(np))*shg(1,i,2)-funcaoExata(xl(1))*shg(1,i,1));
     endif
     
     for j = 1:nen
       if (i == 1 && j == 1)
-        A(i,j) += -(shg(2,j,2)*2/h*1 - shg(2,j,1)*2/h*-1) + alfa*(shg(2,i,2)*2/h*1 - shg(2,i,1)*2/h*-1) + beta*(shg(1,j,2)*1 - shg(1,j,1)*-1);
+        A(i,j)  += -(shge(2,j,2)*2/h*shge(1,i,2) - shge(2,j,1)*2/h*shge(1,i,1)) + alfa*(shge(2,i,2)*2/h*shge(1,j,2) - shge(2,i,1)*2/h*shge(1,j,1)) + beta*(shg(1,j,2)*shg(1,i,2) - shg(1,j,1)*shg(1,i,1));
       endif
       if (i == nen && j == nen)
-        A(np,np) += -(shg(2,j,2)*2/h*1 - shg(2,j,1)*2/h*-1) + alfa*(shg(2,i,2)*2/h*1- shg(2,i,1)*2/h*-1) + beta*(shg(1,j,2)*1 - shg(1,j,1)*-1);
+        A(np,np) += -(shge(2,j,2)*2/h*shge(1,i,2)  - shge(2,j,1)*2/h*shg(1,i,1)) + alfa*(shge(2,i,2)*2/h*shge(1,j,2) - shge(2,i,1)*2/h*shge(1,j,1)) + beta*(shg(1,j,2)*shg(1,i,2) - shg(1,j,1)*shg(1,i,1));
       endif
     endfor
   endfor
-  A
-  F
+
   u = A\F;
   
   %função exata
@@ -89,25 +87,6 @@ for cont = 1:2
     x += h/k;
   endfor
   x = a:h/k:b;
-  
-  %cálculo do erro L2
-  erul2 = 0;
-  for n = 1:nel
-    eru = 0;
-    for l = 1:nint
-      uh = 0;
-      xx = 0;
-      for i = 1:nen
-        uh = uh + shg(1,i,l)*u(k*(n-1)+i);
-        xx = xx + shg(1,i,l)*xl(k*(n-1)+i);
-      endfor
-      eru = eru + ((funcaoExata(xx) - uh)**2) * w(l) * h/2;
-    endfor
-    erul2 = erul2 + eru;
-  endfor
-  erul2 = sqrt(erul2);
-  erro(cont) = erul2;
-  hh(cont) = h;
   
   %salva a resolucao
   nome = sprintf("log/PesosEPontosIntegracao%dalfa%dbeta%d.txt", cont, alfa, beta);
