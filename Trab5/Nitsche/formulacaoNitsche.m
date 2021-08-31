d40bd0a52dad5dbd09cd649cb9c6f4ae1099aa41
Alexandre Vitor <alexandre.braga@estudante.ufjf.br>
2021-08-30 23:13:40 -0300

    nitsche consertado e com os testes

--- Trab5/Nitsche/formulacaoNitsche.m
+++ Trab5/Nitsche/formulacaoNitsche.m
@@ -7,12 +7,6 @@ format long;
 a = 0.00;
 b = 1.00;
 
-##  F(1) += + alfa*(shg(2,nen,2)*2/h*funcaoExata(xl(n+1)) - shg(2,1,1)*2/h*funcaoExata(xl(n))) + beta*(shg(1,nen,2)*funcaoExata(xl(n+1)) - shg(1,1,1)*funcaoExata(xl(n)));
-##  F(np) += + alfa*(shg(2,nen,2)*2/h*funcaoExata(xl(n+1)) - shg(2,1,1)*2/h*funcaoExata(xl(n))) + beta*(shg(1,nen,2)*funcaoExata(xl(n+1)) - shg(1,1,1)*funcaoExata(xl(n)));
-##  A(1,1) += -(shg(2,nen,2)*2/h*shg(1,1,2) - shg(2,nen,1)*2/h*shg(1,1,1)) + alfa*(shg(2,nen,2)*2/h*shg(1,1,2) - shg(2,nen,1)*2/h*shg(1,1,1)) + beta*(shg(1,nen,2)*shg(1,nen,2) - shg(1,1,1)*shg(1,1,1));
-##  A(np,np) += -(shg(2,nen,2)*2/h*shg(1,1,2) - shg(2,nen,1)*2/h*shg(1,1,1)) + alfa*(shg(2,nen,2)*2/h*shg(1,1,2) - shg(2,nen,1)*2/h*shg(1,1,1)) + beta*(shg(1,nen,2)*shg(1,nen,2) - shg(1,1,1)*shg(1,1,1));
-## 
-
 grau = 1;
 alfa = input('Insira o valor de alfa: ');
 beta = input('Insira o valor de beta: ');
@@ -38,9 +32,9 @@ for cont = 1:4
   for i = 2:np
     xl(i) = xl(i-1) + h/k;
   endfor
+  
   %gera shg e pega as funções peso
   [shg, w]= shgGera(nen,nint);
-  [shge] = shgeGera(nen,nint);
 
   %Montagem da Fonte e da Matriz
   for n = 1:nel
@@ -58,21 +52,20 @@ for cont = 1:4
     endfor
   endfor
 
-  %Contorno Nitsche-  
+  %Contorno Nitsche
   for i = 1:nen
     if i == 1
-      F(i) += alfa*(shge(2,i,2)*2/h*funcaoExata(xl(np)) - shge(2,i,1)*2/h*funcaoExata(xl(1)) ) + beta*(funcaoExata(xl(np))*shg(1,i,2)-funcaoExata(xl(1))*shg(1,i,1));
+      F(i) += alfa*(shge(2,i,2)*2/h*funcaoExata(xl(np)) - shge(2,i,1)*2/h*funcaoExata(xl(1))) + beta*(shge(1,i,2)*funcaoExata(xl(np)) - shge(1,i,1)*funcaoExata(xl(1)));
     endif
     if i == nen
-      F(np) += alfa*(shge(2,i,2)*2/h*funcaoExata(xl(np)) - shge(2,i,1)*2/h*funcaoExata(xl(1)) )  + beta*(funcaoExata(xl(np))*shg(1,i,2)-funcaoExata(xl(1))*shg(1,i,1));
+      F(np) += alfa*(shge(2,i,2)*2/h*funcaoExata(xl(np)) - shge(2,i,1)*2/h*funcaoExata(xl(1))) + beta*(shge(1,i,2)*funcaoExata(xl(np)) - shge(1,i,1)*funcaoExata(xl(1)));
     endif
-    
     for j = 1:nen
-      if (i == 1 && j == 1)
-        A(i,j)  += -(shge(2,j,2)*2/h*shge(1,i,2) - shge(2,j,1)*2/h*shge(1,i,1)) + alfa*(shge(2,i,2)*2/h*shge(1,j,2) - shge(2,i,1)*2/h*shge(1,j,1)) + beta*(shg(1,j,2)*shg(1,i,2) - shg(1,j,1)*shg(1,i,1));
+      if i == 1 && j == 1
+        A(i,j) += -(shge(2,j,2)*2/h*shge(1,i,2) - shge(2,j,1)*2/h*shge(1,i,1)) + alfa*(shge(2,i,2)*2/h*shge(1,j,2) - shge(2,i,1)*2/h*shge(1,j,1)) + beta*(shge(1,j,2)*shge(1,i,2) - shge(1,j,1)*shge(1,i,1));
       endif
-      if (i == nen && j == nen)
-        A(np,np) += -(shge(2,j,2)*2/h*shge(1,i,2)  - shge(2,j,1)*2/h*shg(1,i,1)) + alfa*(shge(2,i,2)*2/h*shge(1,j,2) - shge(2,i,1)*2/h*shge(1,j,1)) + beta*(shg(1,j,2)*shg(1,i,2) - shg(1,j,1)*shg(1,i,1));
+      if i == nen && j == nen
+        A(np,np) += -(shge(2,j,2)*2/h*shge(1,i,2) - shge(2,j,1)*2/h*shge(1,i,1)) + alfa*(shge(2,i,2)*2/h*shge(1,j,2) - shge(2,i,1)*2/h*shge(1,j,1)) + beta*(shge(1,j,2)*shge(1,i,2) - shge(1,j,1)*shge(1,i,1));
       endif
     endfor
   endfor
