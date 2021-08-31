2f861f89318abd3bbd8eb04ad09a8a82704b2670
Alexandre Vitor <alexandre.braga@estudante.ufjf.br>
2021-08-30 23:53:47 -0300

    teste inicial lambda

--- Trab5/LambdaDecontinuo/formulacaoLambdaDescontinua.m
+++ Trab5/LambdaDecontinuo/formulacaoLambdaDescontinua.m
@@ -166,35 +166,12 @@ for grau = 1:1
    endfor
    x = a:h/k:b;
       
-##      cálculo do erro L2
-##      erul2 = 0;
-##      for n = 1:nel
-##        eru = 0;
-##        for l = 1:nint
-##          uh = 0;
-##          xx = 0;
-##          for i = 1:nen
-##            uh = uh + shg(1,i,l)*u(i);
-##            xx = xx + shg(1,i,l)*xl(k*(n-1)+i);
-##          endfor
-##          eru = eru + ((funcaoExata(xx) - uh)**2) * w(l) * h/2;
-##        endfor
-##        erul2 = erul2 + eru;
-##      endfor
-##      erul2 = sqrt(erul2);
-##      erro(cont) = erul2;
-##      hh(cont) = h;
-      
    xlU1 = xl(1:end-1);
    xlU2 = xl(2:end);
       
    %salva a resolucao
    nome = sprintf("log/PesosEPontosIntegracao%dalfa%dgrau%d.txt", cont, alfa, grau);
    save(nome, 'alfa', 'beta', 'h', 'xlU1', 'xlU2', 'U', 'x', 'exata');
-    
-    %salva os erros
-    nome = sprintf("log/Erros%d.txt", alfa);
-    save(nome, 'erro', 'hh' );
     
   endfor 
 endfor
